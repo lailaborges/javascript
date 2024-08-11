3b963696f462d094b711b750e923222faa4bf905
lailaborges <lailla.borges@gmail.com>
2024-08-11 20:31:08 -0300

    Problema 2 feito

---
+++ Problema2/script_2.js
@@ -0,0 +1,11 @@
+function maior (a,b){
+    if(a > b){
+        console.log(`O número ${a} é maior que ${b}`);
+    } else if (a < b){
+        console.log(`O número ${a} é menor que ${b}`);
+    } else{
+        console.log(`Os dois números são iguais`);
+    }
+}
+
+maior(6,6);
